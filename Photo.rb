6ea06d1037f3cd8300580aa2865d8b858e21251e
Brian O'Sullivan <brian.osullivan@live.com>
2014-11-14 21:40:15 +0800

    Adding methods to create new filename format

--- Photo.rb
+++ Photo.rb
@@ -8,9 +8,34 @@ class Photo
     @exif = extract_exif_data.exif
   end
 
+  def method_missing(m, *args)
+    if @exif.methods.include?(m)
+      return @exif.send(m)
+    end
+    super.missing_method(m)
+  end
+
+  def new_filename
+    "%4d-%02d-%02d %02d-%02d-%02d %s %s" % [date.year, date.month, date.day, date.hour, date.min, date.sec, @exif.make, @exif.model]
+  end
+
+  def year
+    date.year
+  end
+
+  def month
+    date.month
+  end
+
   private
     def extract_exif_data
       EXIFR::JPEG.new @path
+    end
+
+    def date
+      @exif.date_time
+    rescue ArgumentError
+      @exif.date_time_original
     end
 
 end
